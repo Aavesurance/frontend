c3dcf0e0de152ed25b371046270650450ea65f0d
Vaibhav <vaibhav@pop-os.localdomain>
2021-07-05 20:44:44 +0530

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { useWeb3 } from '@openzeppelin/network/react';
 
 import Web3Data from './components/Web3Data.js';
 
-const infuraProjectId = '50f18860239f4afeaa418afb76717f3c';
+const infuraProjectId = 'de6df6afdb30401f9253f01dde77317d';
 
 function App() {
   const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
